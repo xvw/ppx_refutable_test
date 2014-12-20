866321ee4b444512c456c43c211ed176c40a70cd
Xavier Van de Woestyne <xavier.vdw@gmail.com>
2014-12-20 23:43:02 +0100

    Add example

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 ppx_test
 ========
+Provide a refutable testing environment for OCaml, using **ppx**.
 
-Provide a testing environnement (refutable) for OCaml using -ppx 
+## Introduction
+`ppx_test` add anotation to define tests set 
--- examples/example1.ml
+++ examples/example1.ml
@@ -1,17 +1,31 @@
 (* Example of ppx_test usage*)
 
 module Essay =
-  struct 
-    let succ x = x + 1
-    let pred x = x - 1
-    let is_null x = (x = 0)
-    let divisible_by2 x = (x mod 2) = 1
-  end
+struct 
+  let succ x = x + 1
+  let pred x = x - 1
+  let is_null x = (x = 0)
+  let divisible_by2 x = (x mod 2) = 1
+end
 
-    [@@@test_register
-     let x = 9 in assert(9 = x) in
-     let y = 10 in assert(10 = x)
-    ]
+[@@@test_register
+  assert((Essay.pred 9) == 8)
+]
+[@@@test_register
+  assert(Essay.is_null 0);
+  assert(not (Essay.is_null 1))
+]
+(* Test with value binding*)
+[@@@test_register 
+  let x = 9 in
+  assert((Essay.succ x) = 12) (* this test fail *)
+]
+
+(* Run all tests *)
+[@@@test_process
+  List.map (fun f -> f () ) (registered_tests ())
+]
+
 
 
 
