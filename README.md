0d21b8abbc6c1ea78ec02bba85f5e2fc2b94a697
xvw <xavier.vdw@gmail.com>
2015-11-09 14:15:54 +0100

    Remove old tests tool

--- README.md
+++
@@ -1,4 +0,0 @@
-# ppx_test
-This package implements refutable test context. Each test can be writted inline (into the OCaml file) and ignored or executed.
-
-[A french blog post about ppx_test](http://xvw.github.io/post/ppx_test.html)
--- src/mini_unit.mli
+++
@@ -1,8 +0,0 @@
-module Assert :
-  sig
-    val isTrue : ?verbose:bool -> bool -> unit
-    val isFalse : ?verbose:bool -> bool -> unit
-    val isEquals : ?verbose:bool -> 'a -> 'a -> unit
-    val isNotEquals : ?verbose:bool -> 'a -> 'a -> unit
-  end
-val report : unit -> unit
