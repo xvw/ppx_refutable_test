b7a1c5947f9a8aa13dab71e94ece1a246a10fefc
xvw <xavier.vdw@gmail.com>
2015-11-12 07:23:08 +0100

    Write a most comprehensive sample

--- examples/sample1.ml
+++ examples/sample1.ml
@@ -1,28 +1,33 @@
-(* a Simple example with refutable context *)
+(* Micro sample  
+ * Without ppx_refutable_test, all fragment enclosed into [@@@process ... ]
+ * will be ignored. 
+ * With ppx_refutable_test, all block labelized with [@@refute] will be 
+ * ignored.
+ *)
+
+let is_adult x =
+  if x <= 0 then `Not_born
+  else if x < 18 then `Young
+  else `Ok
+
+[@@@process
+  let test =
+    let _ = assert ((is_adult (-3)) = `Not_born) in
+    let _ = assert ((is_adult 17) = `Young) in
+    assert ((is_adult 26) = `Ok)
+]
+
+let _ =
+  let _ = print_endline "This application containes SEXUAL CONTENT" in
+  let _ = print_endline "What's your age?\n" in
+  let age = read_int () in
+  match is_adult age with
+  | `Not_born -> print_endline "God ! You 'are not born !"
+  | `Young -> print_endline "You're so young !"
+  | `Ok -> print_endline "Welcome on this securised application...\nSEX"
+[@@refute]
 
 
-module type TEST =
-sig
-
-  val t : int -> int -> int
-  
-end
-
-module S(F : TEST) =
-struct
-
-  let t = F.t
-  let tt a b = t (t a b) 10
-
-  [@@@process
-    let _ = print_int (F.t 10 10)
-  ]
-
-end
-
-module R : TEST =
-struct
-  let t = ( + )
-end
-
-module K = S(R)
+[@@@process
+   print_endline "All tests are done"
+]
